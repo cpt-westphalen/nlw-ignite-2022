b8702670a730f2f1c8b2bacdf19d66391fcf8072
rbwestphalen <rbwestphalen@gmail.com>
2022-09-27 11:09:09 -0300

    New: arrows and scroll behaviours and state mgmt

--- web/src/components/GameList.tsx
+++ web/src/components/GameList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { MouseEventHandler, useEffect, useRef, useState } from "react";
 
 import { GameCard } from "./GameCard";
 import { Game } from "../types";
@@ -6,8 +6,12 @@ import { ArrowButton } from "./ArrowButton";
 
 export const GameList = ({ list }: { list: Game[] }) => {
 	const [overflowActive, setOverflowActive] = useState(false);
+	const [atListPosition, setAtListPosition] = useState<
+		"start" | "middle" | "end"
+	>("start");
 
 	const listAreaDiv = useRef<HTMLDivElement>(null);
+	const listAreaContainer = useRef<HTMLDivElement>(null);
 
 	function isOverflowing(element: HTMLElement) {
 		return element.offsetWidth < element.scrollWidth;
@@ -20,27 +24,84 @@ export const GameList = ({ list }: { list: Game[] }) => {
 				: setOverflowActive(false);
 	}, [list]);
 
+	const handleClickLeft: MouseEventHandler = (event): void => {
+		if (
+			listAreaDiv.current &&
+			overflowActive &&
+			listAreaContainer.current
+		) {
+			listAreaDiv.current.scrollLeft -= 240;
+			if (listAreaDiv.current.scrollLeft === 0) {
+				setAtListPosition("start");
+			} else {
+				setAtListPosition("middle");
+			}
+		}
+	};
+	const handleClickRight: MouseEventHandler = (event): void => {
+		if (
+			listAreaDiv.current &&
+			overflowActive &&
+			listAreaContainer.current
+		) {
+			listAreaDiv.current.scrollLeft += 240;
+			if (
+				listAreaDiv.current.offsetWidth +
+					listAreaDiv.current.scrollLeft ===
+				listAreaDiv.current.scrollWidth
+			) {
+				setAtListPosition("end");
+			} else {
+				setAtListPosition("middle");
+			}
+		}
+	};
+
 	return (
 		<div className='flex gap-6 flex-grow-0'>
-			<ArrowButton direction='left' />
+			<ArrowButton
+				direction='left'
+				onClick={handleClickLeft}
+				disabled={
+					overflowActive
+						? atListPosition === "start"
+							? true
+							: false
+						: true
+				}
+			/>
 			<div
-				className='max-w-7xl overflow-hidden flex gap-6 flex-grow-0 flex-shrink-0 relative'
-				ref={listAreaDiv}>
-				{overflowActive && (
+				className='relative'
+				ref={listAreaContainer}>
+				<div
+					className='max-w-7xl overflow-x-scroll flex gap-6 flex-grow-0 flex-shrink-0 container-snap'
+					ref={listAreaDiv}>
+					{list.map((g) => (
+						<a
+							href='#'
+							key={`${g.id}-anchor`}>
+							<GameCard
+								game={g}
+								key={g.id}
+							/>
+						</a>
+					))}
+				</div>
+				{overflowActive && !(atListPosition === "end") && (
 					<div className='absolute w-20 right-0 top-0 bottom-0 bg-gradient-to-l from-[#110D11] to-transparent z-10'></div>
 				)}
-				{list.map((g) => (
-					<a
-						href='#'
-						key={`${g.id}-anchor`}>
-						<GameCard
-							game={g}
-							key={g.id}
-						/>
-					</a>
-				))}
 			</div>
-			<ArrowButton direction='right' />
+			<ArrowButton
+				direction='right'
+				onClick={handleClickRight}
+				disabled={
+					overflowActive
+						? atListPosition === "end"
+							? true
+							: false
+						: true
+				}
+			/>
 		</div>
 	);
 };
