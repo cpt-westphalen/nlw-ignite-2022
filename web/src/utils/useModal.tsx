44ef5cde762300dd8557a1df680d645eda5dd4cf
rbwestphalen <rbwestphalen@gmail.com>
2022-10-03 21:30:32 -0300

    Refactor: minor code clean-up 2

--- web/src/utils/useModal.tsx
+++ web/src/utils/useModal.tsx
@@ -3,13 +3,11 @@ import { useState } from "react";
 import { Modal } from "../types/types";
 
 export const useModal = ({ open, id }: Modal) => {
-	console.log("hook iniciado");
 	const [modalOpen, setModalOpen] = useState(open);
 	const [modalId, setModalId] = useState(id);
 	const modal = { open: modalOpen, id: modalId };
 
 	function changeModal(value: Modal) {
-		console.log(":: changeModal was called! ::");
 		if (value.id !== modal.id) setModalId(value.id);
 		setModalOpen(value.open);
 	}
