b73d7e53f1e52bb6e5ff17ff744a93920abb5545
rbwestphalen <rbwestphalen@gmail.com>
2022-10-05 11:03:39 -0300

    New: Contact shows on button click + clipboard api copy button

--- web/src/components/GameAdsModal/index.tsx
+++ web/src/components/GameAdsModal/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 import { TbDeviceGamepad2 } from "react-icons/tb";
+import { MdOutlineCopyAll } from "react-icons/md";
 
 import {
 	AdTypes,
@@ -149,94 +150,136 @@ const AdsContent = ({ game }: { game: Game; setModal: SetModalType }) => {
 	);
 };
 
-function isRecent(date: string) {
-	// checks if ad was placed less than N days ago.
-	const NUM_OF_DAYS = 7;
-	const now = new Date();
+const AdCard = ({ ad }: { ad: AdTypes }) => {
+	const [showContact, setShowContact] = useState(false);
 
-	const today = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
-	const compareDate = date.split(/\D/).map((v) => parseInt(v));
-
-	const result = () => {
-		if (today[0] === compareDate[0]) {
-			if (today[1] === compareDate[1]) {
-				return today[2] - compareDate[2] < NUM_OF_DAYS;
-			} else if (today[1] - compareDate[1] === 1) {
-				return today[2] + 30 - compareDate[2] < NUM_OF_DAYS;
-			}
-		} else if (today[0] - compareDate[0] === 1) {
-			if (today[1] + 12 - compareDate[1] <= 1) {
-				return today[2] + 30 - compareDate[2] < NUM_OF_DAYS;
-			}
-		}
-		return false;
+	const handleClickContact = (e: React.MouseEvent<HTMLButtonElement>) => {
+		setShowContact(true);
 	};
-	return result();
-}
 
-const AdCard = ({ ad }: { ad: AdTypes }) => (
-	<article className='flex flex-col flex-grow relative min-h-full w-64 my-2 py-3 px-4 flex-shrink-0 gap-2 rounded bg-zinc-900'>
-		{ad.createdAt ? (
-			isRecent(ad.createdAt) ? (
-				<span className='absolute text-sm text-yellow-600 right-3'>
-					Novo!
-				</span>
+	const isRecent = useMemo(() => {
+		// checks if ad was placed less than N days ago.
+		if (ad.createdAt) {
+			const NUM_OF_DAYS = 7;
+			const now = new Date();
+
+			const today = [
+				now.getFullYear(),
+				now.getMonth() + 1,
+				now.getDate(),
+			];
+			const compareDate = ad.createdAt
+				.split(/\D/)
+				.map((v) => parseInt(v));
+
+			const result = () => {
+				if (today[0] === compareDate[0]) {
+					if (today[1] === compareDate[1]) {
+						return today[2] - compareDate[2] < NUM_OF_DAYS;
+					} else if (today[1] - compareDate[1] === 1) {
+						return today[2] + 30 - compareDate[2] < NUM_OF_DAYS;
+					}
+				} else if (today[0] - compareDate[0] === 1) {
+					if (today[1] + 12 - compareDate[1] <= 1) {
+						return today[2] + 30 - compareDate[2] < NUM_OF_DAYS;
+					}
+				}
+				return false;
+			};
+			return result();
+		} else {
+			return false;
+		}
+	}, [ad.createdAt]);
+
+	const handleCopyContact = (e: React.MouseEvent<HTMLButtonElement>) => {
+		navigator.clipboard.writeText(ad.contact).then(() => {
+			console.log("copied!");
+		});
+	};
+
+	return (
+		<article className='flex flex-col flex-grow relative min-h-full w-64 my-2 py-3 px-4 flex-shrink-0 gap-2 rounded bg-zinc-900'>
+			{ad.createdAt ? (
+				isRecent ? (
+					<span className='absolute text-sm text-yellow-600 right-3'>
+						Novo!
+					</span>
+				) : (
+					""
+				)
 			) : (
 				""
-			)
-		) : (
-			""
-		)}
-		<label className='block text-sm text-zinc-400'>
-			Nome
-			<p className='text-base text-white'>{ad.author}</p>
-		</label>
-		<label className='block text-sm text-zinc-400'>
-			Experiência de jogo
-			<p className='text-base text-white'>
-				{ad.experience == 0
-					? "< 1 ano"
-					: `${ad.experience} ano${ad.experience === 1 ? "" : "s"}`}
-			</p>
-		</label>
-		<label className='block text-sm text-zinc-400'>
-			Disponibilidade
-			<p className='text-base text-white'>
-				{ad.days.filter((n) => n !== 0).length > 0
-					? ad.days.map((d) => {
-							let days = "";
-							days +=
-								d !== 0
-									? d == 7
-										? "sáb."
-										: d == 1
-										? "dom."
-										: `${d}ª`
-									: "";
-							if (days) {
-								const isLastDay =
-									d === ad.days[ad.days.length - 1];
-								days += isLastDay ? "" : ", ";
-							}
-							return days;
-					  })
-					: "Não informado"}
-			</p>
-		</label>
-		<label className='block text-sm text-zinc-400'>
-			Chamada de Áudio
-			<p
-				className={`text-base ${
-					ad.voice ? "text-green-600 font-bold" : "text-white"
-				}`}>
-				{ad.voice ? "sim" : "não"}
-			</p>
-		</label>
-		<button className='bg-green-700 hover:bg-green-800 p-2 mt-3 self-center justify-self-end'>
-			Conectar!
-		</button>
-	</article>
-);
+			)}
+			<label className='block text-sm text-zinc-400'>
+				Nome
+				<p className='text-base text-white'>{ad.author}</p>
+			</label>
+			<label className='block text-sm text-zinc-400'>
+				Experiência de jogo
+				<p className='text-base text-white'>
+					{ad.experience == 0
+						? "< 1 ano"
+						: `${ad.experience} ano${
+								ad.experience === 1 ? "" : "s"
+						  }`}
+				</p>
+			</label>
+			<label className='block text-sm text-zinc-400'>
+				Disponibilidade
+				<p className='text-base text-white'>
+					{ad.days.filter((n) => n !== 0).length > 0
+						? ad.days.map((d) => {
+								let days = "";
+								days +=
+									d !== 0
+										? d == 7
+											? "sáb."
+											: d == 1
+											? "dom."
+											: `${d}ª`
+										: "";
+								if (days) {
+									const isLastDay =
+										d === ad.days[ad.days.length - 1];
+									days += isLastDay ? "" : ", ";
+								}
+								return days;
+						  })
+						: "Não informado"}
+				</p>
+			</label>
+			<label className='block text-sm text-zinc-400'>
+				Chamada de Áudio
+				<p
+					className={`text-base ${
+						ad.voice ? "text-green-600 font-bold" : "text-white"
+					}`}>
+					{ad.voice ? "sim" : "não"}
+				</p>
+			</label>
+			{showContact ? (
+				<div className='flex bg-zinc-800 border-solid border-2 rounded-xl border-zinc-600 py-0 px-2 mt-3 self-center justify-self-end justify-center items-center'>
+					<address className='text-sm text-zinc-300'>
+						{ad.contact}
+					</address>
+					<button
+						title='Copiar contato'
+						onClick={handleCopyContact}>
+						<MdOutlineCopyAll size={18} />
+					</button>
+				</div>
+			) : (
+				<button
+					title='Ver informações de contato'
+					onClick={handleClickContact}
+					className='bg-green-700 hover:bg-green-800 p-2 mt-3 self-center justify-self-end'>
+					Conectar!
+				</button>
+			)}
+		</article>
+	);
+};
 
 const CreateAdFallback = () => (
 	<div className='flex flex-col items-center gap-3 my-10 mx-3'>
